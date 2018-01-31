cf7884f65a020b0051ceac4e3e3a607908250928
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2018-01-31 13:33:27 -0200

    Create index js and html template file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Default imports
+import React from 'react';
+import { render } from 'react-dom';
+
+// Main app import
+import App from 'components/app';
+
+// App render
+/* eslint-disable react/jsx-filename-extension */
+render(<App />, document.querySelector('#app'));
+/* eslint-enable no-use-before-define */
